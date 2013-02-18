65ddf3f9fb57d3e6f1cd06d55405bdc1821c693c
Brendan Molloy <brendan@bbqsrc.net>
2013-02-18 18:03:00 +1100

    can now send attachments

--- bbqutils/email.py
+++ bbqutils/email.py
@@ -1,10 +1,14 @@
 import smtplib
+from email import encoders
 from email.message import Message
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.utils import formatdate
 from threading import Lock, Thread
 
 class Mailer:
-	def __init__(self, host, port=587, user=None, passwd=None):
+	def __init__(self, host='localhost', port=587, user=None, passwd=None):
 		self.host = host
 		self.port = port
 		self.user = user
@@ -19,11 +23,11 @@ class Mailer:
 			pass
 		if self.user:
 			self.server.login(self.user, self.passwd)
-	
+
 	def disconnect(self):
 		return self.server.quit()
 
-	def send_email(self, frm=None, to=None, cc=None, bcc=None, subject=None, text=None, date=None, reply_to=None):
+	def send_email(self, frm=None, to=None, cc=None, bcc=None, subject=None, text=None, date=None, reply_to=None, attachments=[]):
 		x = 0
 		while x < 3:
 			x += 1
@@ -34,12 +38,12 @@ class Mailer:
 				to = recipient_list(to)
 				cc = recipient_list(cc)
 				if rl:
-					messages.append((rl, create_email(frm, to, cc, None, subject, text, date, reply_to)))
-		
+					messages.append((rl, create_email(frm, to, cc, None, subject, text, date, reply_to, attachments)))
+
 				if isinstance(bcc, list):
 					for recip in bcc:
-						messages.append((recip, create_email(frm, to, cc, recip, subject, text, date, reply_to)))
-				
+						messages.append((recip, create_email(frm, to, cc, recip, subject, text, date, reply_to, attachments)))
+
 				for m in messages:
 					self.server.sendmail(frm, m[0], m[1].as_string())
 				return
@@ -50,6 +54,15 @@ class Mailer:
 		raise Exception("Failed to send email three times.")
 
 
+def create_attachment(filename, data, mimetype="application/octet-stream"):
+	main, sub = mimetype.split('/')
+	msg = MIMEBase(main, sub)
+	msg.set_payload(data)
+	encoders.encode_base64(msg)
+	msg.add_header('Content-Disposition', 'attachment', filename=filename)
+	return msg
+
+
 def recipient_list(*args):
 	out = []
 	for arg in args:
@@ -58,11 +71,11 @@ def recipient_list(*args):
 		elif isinstance(arg, list):
 			out += arg
 	if len(out) > 0:
-		return "; ".join(out)
+		return ", ".join(out)
 
 
-def create_email(frm=None, to=None, cc=None, bcc=None, subject=None, text=None, date=None, reply_to=None):
-	msg = Message()
+def create_email(frm=None, to=None, cc=None, bcc=None, subject=None, text=None, date=None, reply_to=None, attachments=[]):
+	msg = MIMEMultipart()
 	if frm:
 		msg["From"] = frm
 	if to:
@@ -74,12 +87,15 @@ def create_email(frm=None, to=None, cc=None, bcc=None, subject=None, text=None, 
 	if subject:
 		msg["Subject"] = subject
 	if text:
-		msg.set_payload(text)
+		textmsg = MIMEText(text, _charset='utf-8')
+		msg.attach(textmsg)
 	if date is None:
 		msg["Date"] = formatdate(localtime=True)
 	else:
 		msg["Date"] = formatdate(date, localtime=True)
 	if reply_to:
 		msg["Reply-To"] = reply_to
+	for attachment in attachments:
+		msg.attach(attachment)
 	return msg
 
