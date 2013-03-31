15ea47559a01054a85499adf990487141db089d5
Brendan Molloy <brendan@bbqsrc.net>
2013-03-31 20:01:44 +1100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='bbqutils',
+      version='0.1',
+      description='Brendan\'s utilities',
+      author='Brendan Molloy',
+      author_email='brendan@bbqsrc.net',
+      license="CC0",
+      packages=['bbqutils'],
+     )
